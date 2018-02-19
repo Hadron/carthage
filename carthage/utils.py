7751ad3251d4e85a42f740322a63e729e9825b7c
Sam Hartman <sam.hartman@hadronindustries.com>
2018-02-19 14:38:35 -0500

    memoproperty class

--- carthage/utils.py
+++ carthage/utils.py
@@ -6,7 +6,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
 # LICENSE for details.
 
-import asyncio
+import asyncio, functools
 
 async def possibly_async(r):
     '''If r is a coroutine, await it.  Otherwise return it.  Used like the
@@ -20,4 +20,23 @@ async def possibly_async(r):
         return await r
     else:
         return r
+    
+
+class memoproperty:
+    "A property that only supports getting and that stores the result the first time on the instance to avoid recomputation"
+
+
+    def __init__(self, fun):
+        functools.update_wrapper(self, fun)
+        self.fun = fun
+        self.name = fun.__name__
+
+    def __get__(self, instance, owner):
+        if instance is None: return self
+        #Because we don't define set or del, we should not be called
+        #if name is already set on instance.  So if we set name we
+        #will be bypassed in the future
+        res = self.fun(instance)
+        setattr(instance, self.name, res)
+        return res
     
---
+++ tests/test_utils.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2018, Hadron Industries, Inc.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
+
+from carthage.utils import memoproperty
+
+def test_memo_prop():
+    class m:
+        @memoproperty
+        def foo(self):
+            nonlocal called
+            assert called is False
+            called = True
+            return 99
+    called = False
+    assert isinstance(m.foo, memoproperty)
+    mo = m()
+    assert mo.foo == 99
+    assert mo.foo == 99 #and not called a second time
+    
