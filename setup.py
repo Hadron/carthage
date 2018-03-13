33a40090cc9ffbbe1852ea204d6b83a4c7dafbe6
Sam Hartman <sam.hartman@hadronindustries.com>
2018-03-13 11:14:23 -0400

    Python build system

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from setuptools import setup
+
+setup(
+    name = "carthage",
+    license = "proprietary",
+    maintainer = "Sam Hartman",
+    maintainer_email = "sam.hartman@hadronindustries.com",
+    url = "http://www.hadronindustries.com/",
+    packages = ["carthage", "carthage.hadron"],
+    install_requires = ['pytest', ],
+    scripts = ['bin/carthage-runner'],
+    version = "0.1",
+)
+
