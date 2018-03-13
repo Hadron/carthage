7d1cf627dc1ed526dd2a8eededec631bb303ec11
Sam Hartman <sam.hartman@hadronindustries.com>
2018-03-13 11:14:23 -0400

    Python build system

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# Copyright (C) 2018, Hadron Industries, Inc.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
+
+
+from setuptools import setup
+
+setup(
+    name = "carthage",
+    license = "proprietary",
+    maintainer = "Sam Hartman",
+    maintainer_email = "sam.hartman@hadronindustries.com",
+    url = "http://www.hadronindustries.com/",
+    packages = ["carthage", "carthage.hadron"],
+    install_requires = ['pytest', ],
+    scripts = ['bin/carthage-runner'],
+    version = "0.1",
+)
+
