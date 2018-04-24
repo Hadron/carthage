a749a826a0bc228c2daa004ae2965a2de254b4c3
Sam Hartman <sam.hartman@hadronindustries.com>
2018-04-24 12:52:27 -0400

    Don't include label

--- carthage/vm.py
+++ carthage/vm.py
@@ -154,7 +154,6 @@ class VM(Machine, SetupTaskMixin):
             "port": self.console_port.port,
             "host": sh.hostname('--fqdn', _encoding = 'utf-8').strip(),
             "description": self.full_name,
-            "label": self.full_name,
             "type": "spice",
             "ca": self.vm_ca(),
         }
