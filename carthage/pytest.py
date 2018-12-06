9d19c8dc342f080b63718ccd348bebda3f3d5cda
Sam Hartman <sam.hartman@hadronindustries.com>
2018-12-06 15:56:51 -0500

    Add comment

--- carthage/pytest.py
+++ carthage/pytest.py
@@ -46,7 +46,7 @@ def async_test(t):
     params = list(sig.parameters.values())
     try:
         params = list(filter( lambda p: p.name not in t._injection_dependencies, params))
-    except AttributeError: pass
+    except AttributeError: pass # no @inject call
     param_names = set(p.name for p in params)
     if 'loop' not in param_names:
         orig_loop = False
