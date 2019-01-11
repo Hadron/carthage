03821ea142359471df2920dbca8875a1316a768e
Sam Hartman <sam.hartman@hadronindustries.com>
2019-01-11 17:04:13 -0500

    Update configuration mechanism

---
+++ tests/test_config.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2019, Hadron Industries, Inc.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
+
+import pytest
+from carthage.config import *
+from carthage.dependency_injection import Injector, inject
+
+def test_add_config(ainjector):
+    injector = ainjector.injector
+    config_defaults.add_config({"foo": 33})
+    cl = injector(ConfigLayout)
+    assert cl.foo == 33
+    
+def test_override_config(ainjector):
+    injector = ainjector.injector
+
+    @inject(cl = ConfigLayout)
+    def fn(cl):
+        assert cl.hadron_operations == 99
+    injector.add_provider(config_key("hadron_operations"), 99)
+    injector(fn)
+                          
