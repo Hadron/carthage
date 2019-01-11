516f49c0cacc513390ccecee24f6fc0ef05c8279
Sam Hartman <sam.hartman@hadronindustries.com>
2019-01-11 10:34:12 -0500

    Checkpoint configuration updates
    This is a checkpoint of updating how ConfigLayout works.  I think I'm going to actually take a different direction but wanted to checkpoint what I had so far.

--- carthage/config.py
+++ carthage/config.py
@@ -1,6 +1,61 @@
 import yaml
 from .dependency_injection import inject, Injectable
 
+class ConfigSection:
+
+    def add_config(self, c):
+        '''
+        Add additional config settings.
+
+        :param: c
+            A dictionary of key-value pairs.  The keys will become valid config settings with the value being the initial default.  If the value is a dict, it will be converted to a :class:`ConfigSection`.
+
+'''
+        for k,v in c.items():
+            if hasattr(self, k):
+                orig_val = getattr(self, k)
+                if isinstance(orig_val, ConfigSection):
+                    if isinstance(v, dict):
+                        orig_val.add_config(v)
+                    else:
+                        raise ValueError("{k} needs to be a dictionary to fit into existing ConfigSection".format(k))
+                elif isinstance(v, dict):
+                    cs = ConfigSection()
+                    cs.add_config(v)
+                    setattr(self, k, cs)
+                else: setattr(self, k, v)
+                
+    def _copy(self):
+        new = self.__class__()
+        for k,v in self.__dict__.items():
+            d = None
+            if isinstance(v, ConfigSection):
+                d = v.__dict__
+            elif isinstance(v, dict):
+                d = v
+            if d is not None:
+                cs = ConfigSection()
+                cs.__dict__.update(d)
+                setattr(new, k, cs)
+            else: setattr(new, k, copy.copy(v))
+        return new
+
+    def _load(self, d):
+        for k,v in d.items():
+            try:
+                v_orig = getattr(self, k)
+                if hasattr(v_orig, '__get__'):
+                    raise ValueError('{} cannot be set'.format(k))
+                if isinstance(v_orig, ConfigSection):
+                    if not isinstance(v, dict):
+                        raise ValueError("{} should be a dictionary".format(k))
+                    v_orig._load(v)
+                else:
+                    setattr(self, k, v)
+            except AttributeError:
+                raise AttributeError("{} is not a config attribute".format(k)) from None
+        
+                
 class ConfigLayout(Injectable):
 
     image_dir = "/srv/images/test"
@@ -19,12 +74,13 @@ class ConfigLayout(Injectable):
     def load_yaml(self, y):
         d = yaml.load(y)
         assert isinstance(d,dict)
-        for k,v in d.items():
-            try:
-                v_orig = getattr(self, k)
-                if hasattr(v_orig, '__get__'):
-                    raise ValueError('{} cannot be set'.format(k))
-                setattr(self, k, v)
-            except AttributeError:
-                raise AttributeError("{} is not a config attribute".format(k)) from None
-            
+        self._load(d)
+
+
+    @classmethod
+    def add_config(cls, c):
+        #To work around the fact that the master ConfigLayout is
+        #instantiated late in the game, but ConfigSections are
+        #instantiated as defined.
+        ConfigSection.add_config(cls, c)
+        
