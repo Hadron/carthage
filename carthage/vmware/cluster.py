72742881b7aa9ad52e4b8ca27376c868ad64fea3
Klee Dienes <klee.dienes@hadronindustries.com>
2019-03-18 18:14:21 -0400

    Convert to use self.mob

--- carthage/vmware/cluster.py
+++ carthage/vmware/cluster.py
@@ -17,6 +17,5 @@ class VmwareCluster(VmwareSpecifiedObject, kind='cluster'):
     parent_type = HostFolder
 
     async def do_create(self):
-        assert self.writable
-        self.inventory_object = self.parent.mob.CreateClusterEx(self.name, self.spec)
-        return self.inventory_object
+        self.mob = self.parent.mob.CreateClusterEx(self.name, self.spec)
+        return self.mob
