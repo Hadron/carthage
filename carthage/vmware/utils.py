3538f8d976ead5b894539a79c6eb9d5c7078cd8f
Klee Dienes <klee.dienes@hadronindustries.com>
2019-05-17 09:21:26 -0400

    Include VMware data in TaskError; add TaskIgnoreErrors

--- carthage/vmware/utils.py
+++ carthage/vmware/utils.py
@@ -13,6 +13,26 @@ def wait_for_task(task):
         while task.info.state not in ('success', 'error'):
             time.sleep(0.2)
         if task.info.state == 'error':
-            raise task.info.error
+            class TaskError(type(task.info.error)):
+                def __str__(self):
+                    return f'Error: {super().__str__()}; info: {self.info_str}'
+                def __init__(self,  task):
+                    self.__dict__.update(task.info.error.__dict__)
+                    self.__dict__['info_str'] = str(task.info)
+                    self.__dict__['task'] = task
+                    task.info.__dict__['error'] = None
+            raise TaskError(task)
     return loop.run_in_executor(None, callback)
 
+class TaskIgnoreErrors:
+
+    def __init__(self, *types):
+        self.types = types
+
+    def __enter__(self):
+        print('enter types', self.types)
+        return self
+    def __exit__(self, etype, einstance, etraceback):
+        if etype is not None:
+            for t in self.types:
+                if issubclass(etype, t): return True
