3acfcd57a54a017b7fb0c7448b8a95fca03abc91
Sam Hartman <sam.hartman@hadronindustries.com>
2019-05-23 11:35:47 -0400

    oops

--- carthage/__init__.py
+++ carthage/__init__.py
@@ -38,7 +38,7 @@ try:
     import carthage.hadron.database
     base_injector.add_provider(hadron.database.RemotePostgres)
 except ImportError: pass
-    base_injector.add_provider(carthage.container.ssh_origin, carthage.hadron_layout.test_database_container)
+base_injector.add_provider(carthage.container.ssh_origin, carthage.hadron_layout.test_database_container)
 
 base_injector.add_provider(carthage.container.container_image, carthage.hadron.hadron_container_image)
 base_injector.add_provider(carthage.vm.vm_image, carthage.hadron.hadron_vm_image)
