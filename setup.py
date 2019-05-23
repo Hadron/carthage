d191d6e61fe082d5e374e75cb7b144b6b7fdc2ec
Sam Hartman <sam.hartman@hadronindustries.com>
2019-05-23 11:25:37 -0400

    New version

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setup(
     maintainer = "Sam Hartman",
     maintainer_email = "sam.hartman@hadronindustries.com",
     url = "http://www.hadronindustries.com/",
-    packages = ["carthage", "carthage.hadron"],
+    packages = ["carthage", "carthage.hadron",
+                "carthage.vmware"],
     install_requires = ['pytest', ],
-    scripts = ['bin/carthage-runner'],
+    scripts = ['bin/carthage-runner',
+               'bin/carthage-console',
+               'bin/btrfs-rmrf'],
     package_data = {'carthage': ['resources/templates/*'],
     },
-    version = "0.1",
+    version = "0.3",
 )
 
