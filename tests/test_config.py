c3fd8b9df94be827a68e1c24d7ca74e02f584d45
Sam Hartman <sam.hartman@hadronindustries.com>
2019-06-15 11:10:39 -0400

    Config fixes
    * Allow path substitutions in loaded config files
    
    * Fix getattr_path
    
    * Add a test

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,9 +6,10 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
 # LICENSE for details.
 
-import pytest
+import pytest, os.path
 from carthage.config import *
 from carthage.dependency_injection import Injector, inject
+resource_dir = os.path.dirname(__file__)
 
 def test_add_config(ainjector):
     injector = ainjector.injector
@@ -26,3 +27,10 @@ def test_override_config(ainjector):
     injector.add_provider(config_key("hadron_operations"), 99)
     injector(fn)
                           
+
+def test_substitution_in_yaml(ainjector):
+    injector = ainjector.injector(Injector)
+    cl = injector(ConfigLayout)
+    cl.load_yaml(open(os.path.join(resource_dir, "override-config.yml"),'rt'))
+    assert cl.hadron_operations == "/srv/images/test/hadron-operations"
+    
