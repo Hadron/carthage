bfa36bbf6f254336b56cfc339d9905969cd376df
Sam Hartman <sam.hartman@hadronindustries.com>
2019-06-15 11:28:22 -0400

    Assert on not a type for a config type

--- carthage/config/schema.py
+++ carthage/config/schema.py
@@ -115,6 +115,7 @@ class ConfigSchema(metaclass = ConfigSchemaMeta, prefix = ""):
         __slots__ = ('name', 'type', 'default', 'key')
         
         def __init__(self, name, type, default):
+            assert issubclass(type, type)
             self.name = name
             self.type = type
             self.default = default
