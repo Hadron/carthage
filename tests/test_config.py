f591828e8eb0806b73bf5dfa54291ee9adb844d1
Sam Hartman <sam.hartman@hadronindustries.com>
2019-06-17 15:30:16 -0400

    Make override config test more robust in case of people injecting config layout

--- tests/test_config.py
+++ tests/test_config.py
@@ -12,6 +12,7 @@ def test_add_config(ainjector):
     
 def test_override_config(ainjector):
     injector = ainjector.injector
+    injector.replace_provider(ConfigLayout)
 
     @inject(cl = ConfigLayout)
     def fn(cl):
