75287a7e7f0fa2bdf551b35f84c5238d5c832059
Sam Hartman <sam.hartman@hadronindustries.com>
2019-10-29 10:50:54 -0400

    Fix config types with non-trivial __init__
    Some types like list do important work in __init__.  For example if
    you call list.__new__ but not list.__init__, you get an empty list.
    
    Previously we passed the value into __new__ but not __init__ when
    reading config items from yaml.
    
    Now we pass the value into both.
    
    Also, document the confusing magic value class.
    
    Fixes: #3197

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,4 @@
-import pytest, os.path
+import pytest, os.path, yaml
 from carthage.config import *
 from carthage.dependency_injection import Injector, inject
 resource_dir = os.path.dirname(__file__)
@@ -26,4 +26,16 @@ def test_substitution_in_yaml(ainjector):
     cl = injector(ConfigLayout)
     cl.load_yaml(open(os.path.join(resource_dir, "override-config.yml"),'rt'))
     assert cl.hadron_operations == "/srv/images/test/hadron-operations"
+    
+
+def test_list_in_yaml(ainjector):
+    class ConfigList(ConfigSchema, prefix = ""):
+        l: list = (2,3)
+    injector = ainjector.injector(Injector)
+    cl = injector(ConfigLayout)
+    assert tuple(cl.l) == (2,3)
+    cl.load_yaml(yaml.dump( dict(
+        l = [9,10],
+        )))
+    assert tuple(cl.l) == (9,10)
     
