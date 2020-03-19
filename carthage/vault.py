391906acec4e5cf1ccee288744f26eda0b90b872
Sam Hartman <sam.hartman@hadronindustries.com>
2020-03-19 11:51:45 -0400

    Run with less privilege when configuring vault

--- carthage/vault.py
+++ carthage/vault.py
@@ -6,7 +6,6 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
 # LICENSE for details.
 
-
 import os
 import os.path
 import hvac
@@ -122,6 +121,7 @@ def _apply_policy(client, policy):
             raise VaultError(f"Unable to create Policy {p}")
 
 def _apply_auth(client, auth):
+    if not auth: return
     auth_methods = set(client.sys.list_auth_methods()['data'].keys())
     for a, info in auth.items():
         try:
@@ -136,6 +136,7 @@ def _apply_auth(client, auth):
             raise VaultError(f"Unable to enable auth method at path {a}")
 
 def _apply_secrets(client, secrets):
+    if not secrets: return
     secrets_engines = set(client.sys.list_mounted_secrets_engines()['data'].keys())
     for s, info in secrets.items():
         try:
@@ -152,6 +153,7 @@ def _apply_secrets(client, secrets):
         
             
 def _apply_audit(client, audit):
+    if not audit: return
     audit_devices= set(client.sys.list_enabled_audit_devices()['data'].keys())
     for a, info in audit.items():
         try:
