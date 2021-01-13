c59afc5bda76446ad3b0fdc4a7c780568fefb878
Sam Hartman <sam.hartman@hadronindustries.com>
2021-01-13 14:05:49 -0500

    Always use the exact argument given in injector override
    If we do something like await ainjector(func, arg = foo)
    
    func expects to receive foo as an argument, even if an injector would consider foo a factory, or even if foo is not ready.
    
    This came up in the bootstrapping between SshKey and SshAgent, where
    the key requests an agent, passing itself (a not-ready key) in
    explicitly.  The key will not become ready until the agent does, but
    the agent depends on the key.
    
    For now, if someone queries the key from the subinjector, normal
    resolution will happen.

--- carthage/dependency_injection.py
+++ carthage/dependency_injection.py
@@ -412,13 +412,15 @@ Return the first injector in our parent chain containing *k* or None if there is
                 sub_injector = (type(self))(self)
                 injector = sub_injector
                 for k in kwarg_dependencies:
-                    provider = kwargs.pop(k)
+                    provider = kwargs[k]
                     dependency = cls._injection_dependencies[k]
                     if isinstance(provider,Injectable) and not provider.satisfies_injection_key(dependency):
                         raise UnsatisfactoryDependency(dependency, provider)
                     sub_injector.add_provider(dependency, provider, close = False)
+
             for k, d in (cls._injection_dependencies.items()) if dks else []:
                 if d is None: continue
+                if k in kwargs: continue
                 injector.get_instance(d, placement = kwarg_place(k),
                                       loop = _loop, futures = futures)
             if futures:
