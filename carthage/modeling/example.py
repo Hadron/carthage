eba50c341ba767f349e184d029f1e17375acaf38
Sam Hartman <sam.hartman@hadronindustries.com>
2021-02-11 18:40:30 -0500

    Update the example to be correct for the code

--- carthage/modeling/example.py
+++ carthage/modeling/example.py
@@ -1,36 +1,45 @@
+from .base import *
+from .decorators import *
+from carthage.network import NetworkConfig
+from carthage import InjectionKey
 
 class RouterConfig(NetworkConfigModel):
 
     internet = injector_access("internet")
     site_network = injector_access("site-network")
-    add(internet, 'eth0', None)
-    add(site_network, 'eth1', None)
+    add("eth0", internet, None)
+    add("eth1", site_network,  None)
     
 class Layout(ModelGroup):
 
     class net_config(NetworkConfigModel):
         site_network = injector_access("site-network")
-        add(site_network, 'eth0', None)
+        add("eth0", site_network, None)
         
     class Internet(NetworkModel):
+        
 
-        bridge_name = "brint"
+#        bridge_name = "brint"
+        pass
         
     class Red(Enclave):
         domain = "evil.com"
 
         @provides("site-network")
-        class RedNet(ModelingNetwork):
+        class RedNet(NetworkModel):
             pass
 
 
         class router(MachineModel):
-            add_provider(NetworkConfig, RouterConfig)
+            add_provider(InjectionKey(NetworkConfig), RouterConfig)
 
         class samba(MachineModel):
             add_ansible_role("samba")
-            for u in ('george', 'sue', 'pat'):
-                @dynamic_name(f'{u}_desktop')
-                class desktop(MachineModel):
-                    name = f'${u}-desktop'
-                    
+
+        for u in ('george', 'sue', 'pat'):
+            @dynamic_name(f'{u}_desktop')
+            class desktop(MachineModel):
+                name = f'{u}-desktop'
+        del u
+
+        
