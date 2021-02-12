6b5b1d737cfb756599c5e5ed559d88e8bcd3bcf5
Sam Hartman <sam.hartman@hadronindustries.com>
2021-02-11 20:31:53 -0500

    Automatically injector_access most things
    * Move injector_access to decorators.
    
    * Rename InjectorEntry to NSEntry because it is more a namespace thing
    than an injector thing.
    
    * Use flags rather than ever increasing set of booleans for NSEntry's state.
    
    * By default don't inject_by_name an injector_accesss; I kept getting
      into circular loops with my injector, and also you probably don't
      want to do that.  If you really do, then you can use the provides
      decorator to bring the key back.
    
    * injector_access gains the ability to be transparent as a class property.
    
    * Use the above so that when most types are set in the class
      namespace, they become an injector_access.  This means that when
      accessed as an instance, the object is instantiated, but when
      accessed as a class, you get the class.  There may be some
      adjustment required when these objects are accessed while defining
      the class where they are present.
    
    * Provide decorators for no_close  and disabling the auto injector acess behavior.

--- carthage/modeling/base.py
+++ carthage/modeling/base.py
@@ -1,48 +1,11 @@
 from .implementation import *
+from .decorators import *
 from carthage.dependency_injection import * #type: ignore
 import typing
 import carthage.network
 from .utils import *
 __all__ = []
 
-class injector_access:
-
-    '''Usage::
-
-        val = injector_access("foo")
-
-    At runtime, ``model_instance.val`` will be memoized to ``model_instance.injector.get_instance("foo")``.
-
-    '''
-
-
-    key: InjectionKey
-    name: str
-
-    def __init__(self, key):
-        if not isinstance(key, InjectionKey):
-            key = InjectionKey(key, _ready = False)
-        if key.ready is None: key = InjectionKey(key, _ready = False)
-        #: The injection key to be accessed.
-        self.key = key
-        # Our __call__method needs an injector
-        inject(injector = Injector)(self)
-
-
-    def __get__(self, inst, owner):
-        res = inst.injector.get_instance(self.key)
-        setattr(inst, self.name, res)
-        return res
-
-    def __set_name__(self, owner, name):
-        self.name = name
-
-    def __call__(self, injector: Injector):
-        return injector.get_instance(self.key)
-    def __repr__(self):
-        return f'injector_access({repr(self.key)})'
-
-__all__ += ['injector_access']
 
 @inject(injector = Injector)
 class InjectableModel(Injectable, metaclass = InjectableModelType):
