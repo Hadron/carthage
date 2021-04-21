ef36e131cdc3428f5876e52eb1be9f5517fe775a
Sam Hartman <sam.hartman@hadronindustries.com>
2021-04-21 17:58:05 -0600

    Initial MacStore
    Provide a way to request random MAC addresses for hosts Note that
    currently automatic persistence of the MacStore is not working.

--- carthage/modeling/__init__.py
+++ carthage/modeling/__init__.py
@@ -1,3 +1,4 @@
 from .base import *
 from .decorators import *
 from .ansible import enable_modeling_ansible
+from ..network import persistent_random_mac
--- carthage/network/__init__.py
+++ carthage/network/__init__.py
@@ -3,9 +3,9 @@ from .base import *
 from .base import external_network # Not really sure it should be in all
 __all__ = base.__all__
 
-from .mac import random_mac_addr
+from .mac import random_mac_addr, MacStore, persistent_random_mac
 
-__all__ += ['random_mac_addr']
+__all__ += ['random_mac_addr', 'MacStore', 'persistent_random_mac']
 
 from .config import V4Config
 __all__ += ['V4Config']
