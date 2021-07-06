3c410c0fa04c991c016c8ab66cbee66d0a05b256
Sam Hartman <sam.hartman@hadronindustries.com>
2021-07-06 11:22:15 -0600

    Update for modeling to master

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Copyright (C) 2018, 2019, 2020, Hadron Industries, Inc.
+# Copyright (C) 2018, 2019, 2020, 2021, Hadron Industries, Inc.
 # Carthage is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
 # as published by the Free Software Foundation. It is distributed
@@ -18,6 +18,7 @@ setup(
     url = "http://www.hadronindustries.com/",
     packages = ["carthage", "carthage.hadron",
                 'carthage.config',
+                'carthage.network',
                 'carthage.extra_packages',
                 "carthage.vmware"],
     install_requires = ['pytest', ],
@@ -25,9 +26,10 @@ setup(
                'bin/carthage-console',
                'bin/carthage-vault-tool',
                'bin/btrfs-rmrf'],
-    package_data = {'carthage': ['resources/templates/*'],
+    package_data = {'carthage': ['resources/templates/*',
+                                 'resources/*',],
                     'carthage.extra_packages': ['resources/*'],
     },
-    version = "0.6",
+    version = "0.9",
 )
 
