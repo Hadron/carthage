b7cac0f2beca219f9a7584ee814c204eee91c703
Sam Hartman <hartmans@debian.org>
2021-10-12 09:27:11 -0600

    Add enable_new_vmware_connection

--- carthage/vmware/__init__.py
+++ carthage/vmware/__init__.py
@@ -21,20 +21,31 @@ from . import image
 
 import carthage.vmware.network as network
 
+@carthage.dependency_injection.inject(injector=carthage.Injector)
+def enable_new_vmware_connection(injector):
+    '''
+    Indicate a point in the injector hierarchy where potentially a different connection is required.  Ifg only one connection is required in a layout this function is automatically called when the *carthage.vmware* plugin is enabled.  However, this function can be called to allow for selection of a new connection, datacenter, folder and distributed switch at some point in the injector hierarchy.
+    '''
+    from carthage.dependency_injection import partial_with_dependencies
+    from ..utils import when_needed
+    from . import inventory
+    from .datacenter import VmwareDatacenter
+    injector.add_provider(VmFolder)
+    injector.add_provider(vmware_trunk_key, network._vmware_trunk)
+    injector.add_provider(image.vm_datastore_key, partial_with_dependencies(image.produce_datastore_from_config, "vmware.datastore"))
+    injector.add_provider(VmwareDatacenter)
+    injector.add_provider(VmwareCluster)
+    injector.add_provider(DvSwitch)
+    injector.add_provider(VmwareConnection)
+    injector.add_provider(image.image_datastore_key, partial_with_dependencies(image.produce_datastore_from_config, "vmware.image_datastore"))
+
 @carthage.dependency_injection.inject(
     injector = carthage.dependency_injection.Injector)
 def carthage_plugin(injector):
-    from carthage.dependency_injection import partial_with_dependencies
     from ..utils import when_needed
     from . import inventory
     from .datacenter import VmwareDatacenter
     injector.add_provider(inventory.custom_fields_key, inventory.default_custom_fields)
     injector.add_provider(DistributedPortgroup, allow_multiple = True)
-    injector.add_provider(VmFolder)
-    injector.add_provider(vmware_trunk_key, network._vmware_trunk)
-    injector.add_provider(image.vm_datastore_key, partial_with_dependencies(image.produce_datastore_from_config, "vmware.datastore"), allow_multiple=True)
-    injector.add_provider(VmwareDatacenter)
-    injector.add_provider(VmwareCluster)
-    injector.add_provider(DvSwitch)
-    injector.add_provider(VmwareConnection)
-    injector.add_provider(image.image_datastore_key, partial_with_dependencies(image.produce_datastore_from_config, "vmware.image_datastore"), allow_multiple=True)
+    injector(enable_new_vmware_connection)
+    
