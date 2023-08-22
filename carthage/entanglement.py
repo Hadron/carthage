6cedad9b8b747e923456b42446151b1dca902e49
Sam Hartman <hartmans@debian.org>
2023-08-22 10:04:38 -0600

    Include repr of provided dependencies
    In the entanglement support, provide repr for dependencies:
    * New class ProviderInfo to store the repr
    
    * Link ProviderInfo and ProvidedDependency on provider_id from the
      inspector.
    
    * Generate ProviderInfos in the add_provider callback.

--- carthage/entanglement.py
+++ carthage/entanglement.py
@@ -1,4 +1,4 @@
-# Copyright (C)  2022, Hadron Industries, Inc.
+# Copyright (C)  2022, 2023, Hadron Industries, Inc.
 # Carthage is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
 # as published by the Free Software Foundation. It is distributed
@@ -16,6 +16,8 @@ from entanglement.types import register_type
 from .dependency_injection import InjectionKey, Injector
 
 def encode_injection_key(k):
+    # Note that encoding and decoding an InjectionKey is not an identity.  In particular, the type will become a string.
+    # This may or may not be reasonable depending on how we end up using this code.
     target = k.target
     if isinstance(target, type):
         target = target.__name__
@@ -34,19 +36,23 @@ class CarthageRegistry(SyncStoreRegistry):
     def injector_id(self, injector):
         return id(injector)
     
-    def on_add_provider(self, scope,  other_keys, target_key, **kwargs):
-        if not self.manager: return
+    def on_add_provider(self, scope,  other_keys, target_key, inspector,
+                        **kwargs):
         injector_id = self.injector_id(scope)
         for k in other_keys | {target_key}:
-            o = ProvidedDependency(injector_id=injector_id, key=k)
+            o = ProvidedDependency(injector_id=injector_id, key=k, provider_id=inspector.provider_id)
             self.store_synchronize(o)
+        provider_info = self.get_or_create(ProviderInfo, inspector.provider_id)
+        provider_info.provider_repr = repr(inspector.get_value_no_instantiate())
+        self.store_synchronize(provider_info)
+        
 
     def instrument_injector(self, injector):
         injector.add_event_listener(InjectionKey(Injector), {'add_provider'}, self.on_add_provider)
-        for k in injector.filter(target=None,
-                                 predicate=lambda k:True,
-                                 stop_at=injector):
-            self.on_add_provider(scope=injector, target_key=k, other_keys=set())
+        for k, inspector in injector.inspect():
+            self.on_add_provider(scope=injector, target_key=inspector.key, other_keys=set(),
+                                 inspector=inspector
+                                 )
             
 
 carthage_registry = CarthageRegistry()
@@ -57,8 +63,17 @@ class ProvidedDependency(StoreInSyncStoreMixin):
 
     key: InjectionKey = sync_property(constructor=True)
     injector_id: int = sync_property(constructor=True)
-
+    provider_id: int = sync_property(None)
 
     sync_registry = carthage_registry
     sync_primary_keys = ('injector_id', 'key')
 
+@dataclasses.dataclass
+class ProviderInfo(StoreInSyncStoreMixin):
+
+    id: int = sync_property(constructor=True)
+    provider_repr:str = sync_property(None)
+
+    sync_registry = carthage_registry
+    sync_primary_keys = ('id',)
+    
