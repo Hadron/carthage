c890ed4eb15237486139cfe3f7e719c4f8a54ce9
Sam Hartman <hartmans@debian.org>
2023-10-23 19:31:50 -0600

    Add license to new file

--- carthage/entanglement/__init__.py
+++ carthage/entanglement/__init__.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2022,  2023, Hadron Industries, Inc.
+# Carthage is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License version 3
+# as published by the Free Software Foundation. It is distributed
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
+# LICENSE for details.
+
 from carthage.config import ConfigSchema
 from .server import CarthageEntanglement, carthage_plugin
 from .instrumentation import *
