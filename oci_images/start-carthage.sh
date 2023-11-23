e8a52dce853bc1ee6d3334d3034a3b76d27831f8
Sam Hartman <hartmans@debian.org>
2023-11-22 20:36:52 -0700

    Carthage container: install dependencies

--- oci_images/start-carthage.sh
+++ oci_images/start-carthage.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 export PYTHONPATH=/carthage
 cd /carthage
-test -f /layout/config.yml &&exec ./bin/carthage-runner --tmux --generate --config /layout/config.yml
+runner_config=
+test -f /layout/carthage_plugin.yml && runner_config="--plugin /layout"
+# The next line overrides: we prefer config.yml to plugin
+test -f /layout/config.yml && runner_config="--config /layout/config.yml"
+if [ "$runner_configx" != "x" ]; then
+    apt update
+    /carthage/bin/carthage $runner_config install_dependencies
+    exec /carthage/bin/carthage-runner $runner_config --generate --keep --tmux
+   fi
 exec ./bin/carthage-console
