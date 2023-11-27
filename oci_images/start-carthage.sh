29f4b3a56a65a80190e708bec9cf8fc9cb239c77
Sam Hartman <hartmans@debian.org>
2023-11-27 09:58:02 -0700

    Include /layout as a plugin even if there is a config.yml in case the config.yml does not include the layout as a plugin.

--- oci_images/start-carthage.sh
+++ oci_images/start-carthage.sh
@@ -3,8 +3,7 @@ export PYTHONPATH=/carthage
 cd /carthage
 runner_config=
 test -f /layout/carthage_plugin.yml && runner_config="--plugin /layout"
-# The next line overrides: we prefer config.yml to plugin
-test -f /layout/config.yml && runner_config="--config /layout/config.yml"
+test -f /layout/config.yml && runner_config="--config /layout/config.yml $runner_config"
 if [ "$runner_configx" != "x" ]; then
     apt update
     /carthage/bin/carthage $runner_config install_dependencies
