318ef5d00f74f621a77cf9c86fc59ae33063248f
Sam Hartman <hartmans@debian.org>
2024-01-16 09:20:35 -0700

    Fix start.sh
    To bring up console if runner_config is empty

--- oci_images/start-carthage.sh
+++ oci_images/start-carthage.sh
@@ -4,7 +4,7 @@ cd /carthage
 runner_config=
 test -f /layout/carthage_plugin.yml && runner_config="--plugin /layout"
 test -f /layout/config.yml && runner_config="--config /layout/config.yml $runner_config"
-if [ "$runner_configx" != "x" ]; then
+if [ "${runner_config}x" != "x" ]; then
     apt update
     /carthage/bin/carthage $runner_config install_dependencies
     exec /carthage/bin/carthage-runner $runner_config --generate --keep --tmux
