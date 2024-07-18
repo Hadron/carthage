ad8558efba380d12fd4361179ca2821291985a2e
Sam Hartman <hartmans@debian.org>
2024-07-18 14:52:57 -0600

    Tests for Plugin mappings

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021, Hadron Industries, Inc.
+# Copyright (C) 2021, 2024, Hadron Industries, Inc.
 # Carthage is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License version 3
 # as published by the Free Software Foundation. It is distributed
@@ -7,9 +7,10 @@
 # LICENSE for details.
 
 import pytest
+import yaml
 from carthage.pytest import *
 from carthage.dependency_injection import *
-from carthage.plugins import load_plugin
+from carthage.plugins import load_plugin, PluginMappings, _parse_plugin_spec
 from pathlib import Path
 
 
@@ -27,3 +28,17 @@ def test_load_vmware(injector):
 
 def test_dir_plugin(injector):
     injector(load_plugin, dir / "test_plugin")
+
+def test_plugin_mappings(injector):
+    test_data_path = dir/'test_plugin_mappings.yml'
+    test_data = yaml.safe_load(test_data_path.read_text())
+    for environment in test_data:
+        plugin_mappings =  injector(PluginMappings)
+        for mapping in environment['mappings']:
+            plugin_mappings.add_mapping(mapping)
+    for test in environment['tests']:
+        spec = _parse_plugin_spec(test['spec'])
+        mapped = plugin_mappings.map(spec)
+        expected = _parse_plugin_spec(test['expected'])
+        assert mapped == expected, f'Unexpected result mapping {spec}'
+        
