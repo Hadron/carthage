c1b57e0db73e4dfdf995d27df2afab79fa04f9bc
Sam Hartman <hartmans@debian.org>
2025-01-09 12:53:50 -0700

    Handle monkey patching __await__ if return_cmd not handled

--- carthage/sh.py
+++ carthage/sh.py
@@ -16,9 +16,18 @@ Also, by default for commands retrieved through this module set _bg=True and _bg
 '''
 try:
     # Setting _async to true doesn't do much except it tends to override _bg, and too much of our code gets confused by that.
-    _sh_context = _sh.bake(_return_cmd=True, _bg=True)
+    _sh_context = _sh.bake(_return_cmd=True, _bg=True, _bg_exc=False)
+    async def test_return_cmd():
+        c = await _sh_context.ls(_async=True, _return_cmd=True)
+        if not isinstance(c, _sh.RunningCommand):
+            import warnings
+            warnings.warn('This sh is too old to properly handle _async _return_cmd=True')
+            return True
+        return False
+    force_override_await = _asyncio.get_event_loop().run_until_complete(test_return_cmd())
 except AttributeError:
     _sh_context = _sh(_bg=True, _bg_exc=False)
+    force_override_await = False
 
 def __getattr__(name):
     val = getattr(_sh_context, name)
@@ -30,6 +39,7 @@ def running_command_await(self):
     res =  yield from loop.run_in_executor(None, self.wait)
     return res
 
-if not hasattr(_sh.RunningCommand, '__await__'):
+if not hasattr(_sh.RunningCommand, '__await__') or force_override_await:
     _sh.RunningCommand.__await__ = running_command_await
 del running_command_await
+del force_override_await
