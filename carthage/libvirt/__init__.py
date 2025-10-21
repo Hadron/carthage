88142703cc3faa09faebb3fbc097bfa9c0bd4d79
Spencer Rak <spencer.rak@hadronindustries.com>
2025-10-21 07:00:54 -0400

    Add configuration options

--- carthage/libvirt/__init__.py
+++ carthage/libvirt/__init__.py
@@ -18,11 +18,45 @@ class LibvirtSchema(ConfigSchema, prefix='libvirt'):
     preferred_format: str = 'raw'
 
     #: When creating a format like qcow2 that can be represented as a
-    #delta on top of another file, should we use such a backing
-    #file. If true, then that file must remain unmodified. Generally
-    #it is better to use OS-level facilities like reflinks to obtain
-    #copy-on-write.
+    # delta on top of another file, should we use such a backing
+    # file. If true, then that file must remain unmodified. Generally
+    # it is better to use OS-level facilities like reflinks to obtain
+    # copy-on-write.
     use_backing_file: bool = False
+
+    #: Set whether Carthage defines libvirt domains by default
+    # defaults to not defining domains
+    # may be overridden on models
+    should_define: bool = False
+
+    #: Default disk size in mebibytes
+    # defaults to 10GiB
+    # may be overridden on models
+    image_size_mib: int = 10485760
+
+    #: Default image location
+    # defaults to a place libvirt can access
+    image_dir: str = "/srv/carthage/libvirt"
+
+    #: Default vm memory in MB
+    # defaults to 2G
+    # may be overridden on models
+    memory_mb: int = 2048
+
+    #: Default vm vcpu count
+    # defaults to 2
+    # may be overridden on models
+    cpus: int = 2
+
+    #: Default image to use
+    # Defaults to None, which will raise an error if not provided elsewhere
+    # MUST be set in the config, layout, or on the model
+    # this is a fallback provided for convenience
+    image: str = None
+
+    #: Is Carthage running on the hypervisor
+    # may be overridden in the layout
+    local_hypervisor: bool = False
 
 class LibvirtDeployableFinder(deployment.DeployableFinder):
 
