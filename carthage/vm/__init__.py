4382620d25593d6bdbc5c2528951540cf8653671
Spencer Rak <spencer.rak@hadronindustries.com>
2025-10-24 19:14:43 -0400

    Clarify where the import came from

--- carthage/vm/__init__.py
+++ carthage/vm/__init__.py
@@ -6,11 +6,19 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the file
 # LICENSE for details.
 
+import inspect
+def get_caller():
+    for sf in inspect.stack():
+        if sf.filename.startswith("<"): continue
+        if sf.filename.endswith("/carthage/vm/__init__.py"): continue
+        return (sf.filename, sf.lineno)
+file, line = get_caller()
 import warnings
 warnings.filterwarnings("default", category=DeprecationWarning, module=__name__)
-warnings.warn("\n\n\
+warnings.warn(f"\n\n\
 =============================================\n\
 carthage.vm has migrated to carthage.libvirt.\n\
+File: '{file}' has imported carthage.vm at line: {line}\n\
 Please import carthage.libvirt in the future.\n\
 =============================================\n\
 ", category=DeprecationWarning)
