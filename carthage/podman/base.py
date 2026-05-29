f87d0c9f59dfc3b66ccc7e32c15c3cb110e6f502
Sam Hartman <hartmans@debian.org>
2026-05-29 06:55:48 -0600

    Quote image labels

--- carthage/podman/base.py
+++ carthage/podman/base.py
@@ -856,7 +856,7 @@ class PodmanImage(OciImage, SetupTaskMixin, no_auto_inject=True):
         if entrypoint:
             options.append('--change=ENTRYPOINT ' + entrypoint)
         for key, value in self.oci_labels.items():
-            options.append(f'--change=LABEL {key}={value}')
+            options.append(f'--change=LABEL {key}={json.dumps(value)}')
         for k, v in self.injector.filter_instantiate(
                 OciEnviron, lambda k: 'name' in k.constraints and k.constraints.get('scope', 'all') in ('all','image')):
             options.append('--change=ENV '+v.assignment)
@@ -1184,7 +1184,7 @@ class ContainerfileImage(OciImage, no_auto_inject=True):
         if layout_name := layout and layout.layout_name:
             options.extend(['--label', 'carthage.layout='+layout_name])
         for key, value in self.oci_labels.items():
-            options.extend(['--label', f'{key}={value}'])
+            options.extend(['--label', f'{key}={json.dumps(value)}'])
         # Instantiate a container simply so we can ask it for volume, mount, and environment options.
         with instantiation_not_ready():
             container = await self.ainjector(PodmanContainer, name='image_options', oci_container_image=self)
